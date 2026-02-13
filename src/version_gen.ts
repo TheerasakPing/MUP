e4d1b367f8d321c34499a2f209d0932ba4b93f21
TheerasakPing <theerasak.shadow@gmail.com>
2026-02-13 16:03:12 +0700

    chore: update version metadata for v0.17.8

--- src/version_gen.ts
+++ src/version_gen.ts
@@ -2,7 +2,7 @@
 // Do not edit manually
 
 export const VERSION = {
-  git_commit: "a996837",
-  git_describe: "v0.17.7-2-ga996837-dirty",
-  buildTime: "2026-02-13T08:56:28Z",
+  git_commit: "574739a",
+  git_describe: "v0.17.8",
+  buildTime: "2026-02-13T09:03:02Z",
 };
